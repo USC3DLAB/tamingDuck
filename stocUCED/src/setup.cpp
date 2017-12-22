6f06ae86f051d08a3ce0ce543c4c097c9c855028
Semih Atakan <atakan@usc.edu>
2017-12-21 16:17:41 -0800

    time visualization are working now (not perfectly sure how).

--- stocUCED/src/setup.cpp
+++ stocUCED/src/setup.cpp
@@ -25,10 +25,17 @@ extern runType runParam;
  */
 int setup_DUCDED(PowSys &powSys, StocProcess &stocProc) {
 	int h, t, n;
-	tm nowTime;
-
+	
+	/* time visualization */
+	time_t rawTime;
+	struct tm * timeInfo;
+	
+	time ( &rawTime );
+	timeInfo = localtime( &rawTime );
+	
 	string timeStamp = getCurrentDateTime();
-
+	
+	/* initializations */
 	// TODO: Move this initialization to a better place.
 	vector<string> detElems (1);
 	detElems[0] = "Load";
@@ -50,10 +57,11 @@ int setup_DUCDED(PowSys &powSys, StocProcess &stocProc) {
 		Solution soln;
 		soln.allocateMem(powSys.numGen, runParam.DA_horizon/runParam.baseTime);
 
-		int beginPeriod = 0; nowTime.tm_hour = 0; nowTime.tm_min = 0; mktime(&nowTime);
+		int beginPeriod = 0; 	timeInfo->tm_min = 0;	timeInfo->tm_hour = 0; mktime(timeInfo);
+
 		/* Long-term unit commitment */
 		for ( h = 0; h < runParam.DA_numSolves; h++ ) {
-			printf("Long-term Unit-Commitment (%02d:%02d): ", nowTime.tm_hour, nowTime.tm_min);
+			printf("Long-term Unit-Commitment (%02d:%02d): ", timeInfo->tm_hour, timeInfo->tm_min);
 
 			UCmodel DAmodel;
 			DAmodel.formulate(inst, DayAhead, Transmission, 0, rep);
@@ -62,7 +70,7 @@ int setup_DUCDED(PowSys &powSys, StocProcess &stocProc) {
 
 			/* Short-term unit commitment */
 			for ( t = 0; t < runParam.ST_numSolves; t++ ) {
-				printf("\tShort-term Unit-Commitment (%02d:%02d): ", nowTime.tm_hour, nowTime.tm_min);
+				printf("\tShort-term Unit-Commitment (%02d:%02d): ", timeInfo->tm_hour, timeInfo->tm_min);
 				UCmodel STmodel;
 				STmodel.formulate(inst, ShortTerm, Transmission, 0, rep);
 				STmodel.solve();
@@ -70,15 +78,15 @@ int setup_DUCDED(PowSys &powSys, StocProcess &stocProc) {
 
 				/* Economic dispatch */
 				for ( n = 0; n < runParam.ED_numSolves; n++ ) {
-					printf("\t\tEconomic Dispatch (%02d:%02d): ", nowTime.tm_hour, nowTime.tm_min);
+					printf("\t\tEconomic Dispatch (%02d:%02d): ", timeInfo->tm_hour, timeInfo->tm_min);
 					EDmodel DED(inst, beginPeriod, rep);
 					DED.formulate(inst, beginPeriod);
 					DED.solve(inst, beginPeriod);
 					cout << "Success." << endl;
 
 					/* Move to the next period */
-					nowTime.tm_min += runParam.baseTime;
-					mktime(&nowTime);
+					timeInfo->tm_min += runParam.baseTime;
+					mktime(timeInfo);
 					beginPeriod ++;
 				}
 			}
