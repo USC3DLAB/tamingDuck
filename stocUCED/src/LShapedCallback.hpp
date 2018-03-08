708c3450c071037d708529670908e6a4e320f193
Semih Atakan <atakan@usc.edu>
2018-03-08 15:57:12 -0800

    Releasing Generic-Callback version of the L-shaped.

---
+++ stocUCED/src/LShapedCallback.hpp
@@ -0,0 +1,60 @@
+//
+//  LShapedCallback.hpp
+//  tamingDuck
+//
+//  Created by Semih Atakan on 3/8/18.
+//  Copyright © 2018 Semih Atakan. All rights reserved.
+//
+
+#ifndef LShapedCallback_hpp
+#define LShapedCallback_hpp
+
+#include <map>
+#include <vector>
+#include <stdio.h>
+#include <ilcplex/ilocplex.h>
+#include "ilconcert/ilothread.h"		// for mutex (must be included below boost libraries to avoid errors)
+
+using namespace std;
+
+class SUCmaster;
+
+class LShapedCallback: public IloCplex::Callback::Function {
+public:
+	// constructor
+	LShapedCallback (SUCmaster &master);
+	
+	// destructor
+	virtual ~LShapedCallback () {}
+	
+	// when to invoke the callback? this function will tell you
+	virtual void invoke (const IloCplex::Callback::Context &context);
+	
+	// L-shaped cut generator
+	inline void addLShapedCuts (const IloCplex::Callback::Context &context);
+	
+	// searches solutions in the solution list
+	inline map<vector<bool>, double>::iterator findSolution(const IloCplex::Callback::Context &context);
+	
+private:
+	// empty constructor
+	LShapedCallback () {}
+	
+	// copy constructor
+	LShapedCallback (const LShapedCallback &tocopy);
+	
+	// pointer to the master problem
+	SUCmaster *master;
+	
+	// tolerances
+	const double AbsOptTol = 1e-6;
+	const double RelOptTol = 1e-4;
+	
+	// parallel thread management
+	IloFastMutex mutex;
+	
+	// solution management
+	map<vector<bool>, double> solMap;
+};
+
+#endif /* LShapedCallback_hpp */
