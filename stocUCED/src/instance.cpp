9f67b45603fc5de6c7d5704f54b303e441132e4e
Semih Atakan <atakan@usc.edu>
2018-04-04 19:15:49 -0700

    Forecast updates.

--- stocUCED/src/instance.cpp
+++ stocUCED/src/instance.cpp
@@ -53,13 +53,13 @@ bool instance::initialize(PowSys *powSys, StocProcess *stoc, string RScriptsPath
 	boostRenewableSupply(actuals);
 	correctSupplyExceedingCapacity(actuals);
 	
-	// print out summary
-	cout << actuals.name << ":" << endl;
-	int i=0;
-	for (auto it=actuals.mapVarNamesToIndex.begin(); it!=actuals.mapVarNamesToIndex.end(); ++it, ++i) {
-		cout << setw(4) << left << i << setw(30) << it->first << " (" << setprecision(2) << runParam.renewableCoef << "x)\t[OK]" << endl;
-	}
-	cout << endl;
+//	// print out summary
+//	cout << actuals.name << ":" << endl;
+//	int i=0;
+//	for (auto it=actuals.mapVarNamesToIndex.begin(); it!=actuals.mapVarNamesToIndex.end(); ++it, ++i) {
+//		cout << setw(4) << left << i << setw(30) << it->first << " (" << setprecision(2) << runParam.renewableCoef << "x)\t[OK]" << endl;
+//	}
+//	cout << endl;
 
 	/* mean forecasts */
 	filename = "DA";
@@ -81,13 +81,13 @@ bool instance::initialize(PowSys *powSys, StocProcess *stoc, string RScriptsPath
 	boostRenewableSupply(meanForecast[filename]);
 	correctSupplyExceedingCapacity(meanForecast[filename]);
 	
-	// print out summary
-	cout << meanForecast[filename].name << ":" << endl;
-	i=0;
-	for (auto it=meanForecast[filename].mapVarNamesToIndex.begin(); it!=meanForecast[filename].mapVarNamesToIndex.end(); ++it, ++i) {
-		cout << setw(4) << left << i << setw(30) << it->first << " (" << setprecision(2) << runParam.renewableCoef << "x)\t[OK]" << endl;
-	}
-	cout << endl;
+//	// print out summary
+//	cout << meanForecast[filename].name << ":" << endl;
+//	i=0;
+//	for (auto it=meanForecast[filename].mapVarNamesToIndex.begin(); it!=meanForecast[filename].mapVarNamesToIndex.end(); ++it, ++i) {
+//		cout << setw(4) << left << i << setw(30) << it->first << " (" << setprecision(2) << runParam.renewableCoef << "x)\t[OK]" << endl;
+//	}
+//	cout << endl;
 
 	// if updated forecasts are requested, initialize its structure
 	if (runParam.updateForecasts) {
@@ -134,7 +134,7 @@ void instance::summary() {
  ****************************************************************************/
 void instance::simulateScenarios(int numScen, bool fitModel, int rep) {
 	// clear the previous info, if there is any
-	simulations.clear();
+	simulations.clear();	// TODO: This clears the deterministic elements (load) as well.
 	
 	int maxLookAhead = max(runParam.ED_numPeriods-1, runParam.ST_numPeriods-1);
 	int numSimLengthInDays = ceil( (double)(runParam.numPeriods+maxLookAhead)/(60.0/runParam.baseTime) );
@@ -161,76 +161,97 @@ void instance::simulateScenarios(int numScen, bool fitModel, int rep) {
 	}
 }
 
-void instance::updateForecasts(vector<double> &futureInfoWeights, int rep, int beginMin, int endMin) {
-	ScenarioType* DAForecasts = &meanForecast["DA"];
-	ScenarioType* RTForecasts = &meanForecast["RT"];
+void instance::updateForecasts(int rep, int beginMin, int endMin) {
 	
-	ScenarioType* DASimulations = simulations.size() > 0 ? &simulations["DA"] : NULL;
-	ScenarioType* RTSimulations = simulations.size() > 0 ? &simulations["RT"] : NULL;
+	// initializations
+	const vector<double> weights = {
+		0.900, 0.875, 0.850, 0.825,
+		0.800, 0.775, 0.750, 0.725,
+		0.700, 0.675, 0.650, 0.625,
+		0.600, 0.575, 0.550, 0.525,
+		0.500, 0.500, 0.500, 0.500};
 	
+	int DAIdx, RTIdx;
 	int beg = beginMin/runParam.baseTime;	// current period
 	int end = endMin/runParam.baseTime;		// period until updates will be done
 	
-	int DAIdx, RTIdx;
+	if (beg <= 0) return;	// no updates when no realization is available
+//	if (end >= runParam.numPeriods) end = runParam.numPeriods-1;
+	
+	ScenarioType* DAForecasts = &meanForecast["DA"];
+	ScenarioType* RTForecasts = &meanForecast["RT"];
+
+	ScenarioType* DASimulations = simulations.size() > 0 ? &simulations["DA"] : NULL;
+	ScenarioType* RTSimulations = simulations.size() > 0 ? &simulations["RT"] : NULL;
+	
+	// forecast updates
 	for (auto it = actuals.mapVarNamesToIndex.begin(); it != actuals.mapVarNamesToIndex.end(); ++it) {
-		// get the correct indices
-		DAIdx = DAForecasts->mapVarNamesToIndex[ it->first ];
-		RTIdx = RTForecasts->mapVarNamesToIndex[ it->first ];
-		
-		// update point-forecasts
-		for (int t=beg; t<=end; t++) {
-			RTForecasts->vals[rep][t][RTIdx] = actuals.vals[rep][t][it->second] * futureInfoWeights[t-beg] + DAForecasts->vals[rep][t][DAIdx] * (1-futureInfoWeights[t-beg]);
-		}
-		
-		// update simulations
-		if (simulations.size() > 0) {
-			// get the correct indices
-			DAIdx = DASimulations->mapVarNamesToIndex[ it->first ];
-			RTIdx = RTSimulations->mapVarNamesToIndex[ it->first ];
+		// make the updates
+		for (int t=beg; t<=end; t++) {			
+			// get the indices associated with it->second (might be different if maps are filled in diff order)
+			DAIdx = DAForecasts->mapVarNamesToIndex[ it->first ];
+			RTIdx = RTForecasts->mapVarNamesToIndex[ it->first ];
 			
-			// update the simulations
-			for (int s=0; s<runParam.numTotScen; s++) {
-				for (int t=beg; t<=end; t++) {
-					RTSimulations->vals[s][t][RTIdx] = actuals.vals[rep][t][it->second] * futureInfoWeights[t-beg] + DASimulations->vals[s][t][DAIdx] * (1-futureInfoWeights[t-beg]);
+			// point-forecasts
+			RTForecasts->vals[rep][t][RTIdx] = actuals.vals[rep][beg-1][it->second] + DAForecasts->vals[rep][t][DAIdx] - DAForecasts->vals[rep][t-1][DAIdx];
+			RTForecasts->vals[rep][t][RTIdx] = RTForecasts->vals[rep][t][RTIdx] * weights[t-beg] + DAForecasts->vals[rep][t][DAIdx] * (1-weights[t-beg]);
+			
+			if (RTForecasts->vals[rep][t][RTIdx] < 0)	RTForecasts->vals[rep][t][RTIdx] = 0.0;
+			
+			// simulations
+			if (simulations.size() > 0) {
+				// get the correct indices
+				DAIdx = DASimulations->mapVarNamesToIndex[ it->first ];
+				RTIdx = RTSimulations->mapVarNamesToIndex[ it->first ];
+				
+				// update the simulations
+				for (int s=0; s<runParam.numTotScen; s++) {
+					for (int t=beg; t<=end; t++) {
+						RTSimulations->vals[s][t][RTIdx] = actuals.vals[rep][beg-1][it->second] + DASimulations->vals[s][t][DAIdx] - DASimulations->vals[s][t][DAIdx];
+						RTSimulations->vals[rep][t][RTIdx] = RTSimulations->vals[rep][t][RTIdx] * weights[t-beg] + DASimulations->vals[rep][t][DAIdx] * (1-weights[t-beg]);
+						
+						if (RTSimulations->vals[rep][t][RTIdx] < 0)	RTSimulations->vals[rep][t][RTIdx] = 0.0;
+					}
 				}
 			}
 		}
 	}
 	
-	//	double actualValueWeight = 0.5;
-	//	double actualTrendWeight = 0.5;
-	//
-	//	double latestValue, latestAvgTrend, DAForecast, DATrend = 0.0;
-	//
-	//	for (auto genItr = powSys->generators.begin(); genItr != powSys->generators.end(); ++genItr) {	// iterate (only) over solar.wind generators, exc. loads
-	//		if (genItr->type != Generator::SOLAR && genItr->type != Generator::WIND) continue;
-	//
-	//		// get the latest available actual data, and a 3-period averaged trend.
-	//		latestValue = actuals.vals[rep][beg-1][ actuals.mapVarNamesToIndex[genItr->name] ];
-	//		latestAvgTrend = (latestValue - actuals.vals[rep][beg-4][ actuals.mapVarNamesToIndex[genItr->name] ])/3.0;
-	//
-	//		ofstream output;
-	//		output.open("plot.txt", ios::app);
-	//		for (int t=beg; t<=end; t++) {	// iterate over forecasting horizon
-	//			// get the desired period's forecast and trend
-	//			DAForecast = DAForecasts->vals[rep][t][ DAForecasts->mapVarNamesToIndex[genItr->name] ];
-	//			DATrend = DATrend*0.25 + (DAForecast - DAForecasts->vals[rep][t-1][DAForecasts->mapVarNamesToIndex[genItr->name]])*0.75;
-	//
-	//			// compute the forecast
-	//			meanForecast["RT"].vals[rep][t][ meanForecast["RT"].mapVarNamesToIndex[genItr->name] ] = max(0.0, latestValue*actualValueWeight + DAForecast*(1-actualValueWeight) + latestAvgTrend*actualTrendWeight + DATrend*(1-actualTrendWeight) );
-	//			if ( genItr->name.compare("Solar-bus015") == 0 ) {
-	//				output << meanForecast["RT"].vals[rep][t][ meanForecast["RT"].mapVarNamesToIndex[genItr->name] ] << endl;
-	//			}
-	//		}
-	//		output.close();
-	//	}
-}
-
-
-void instance::updateForecasts(double futureInfoWeight, int rep, int beginMin, int endMin) {
-	int nbPeriods = (endMin - beginMin)/runParam.baseTime + 1;
-	vector<double> weights (nbPeriods, futureInfoWeight);
-	updateForecasts(weights, rep, beginMin, endMin);
+	
+//	ScenarioType* DAForecasts = &meanForecast["DA"];
+//	ScenarioType* RTForecasts = &meanForecast["RT"];
+//
+//	ScenarioType* DASimulations = simulations.size() > 0 ? &simulations["DA"] : NULL;
+//	ScenarioType* RTSimulations = simulations.size() > 0 ? &simulations["RT"] : NULL;
+//
+//	int beg = beginMin/runParam.baseTime;	// current period
+//	int end = endMin/runParam.baseTime;		// period until updates will be done
+//
+//	int DAIdx, RTIdx;
+//	for (auto it = actuals.mapVarNamesToIndex.begin(); it != actuals.mapVarNamesToIndex.end(); ++it) {
+//		// get the correct indices
+//		DAIdx = DAForecasts->mapVarNamesToIndex[ it->first ];
+//		RTIdx = RTForecasts->mapVarNamesToIndex[ it->first ];
+//
+//		// update point-forecasts
+//		for (int t=beg; t<=end; t++) {
+//			RTForecasts->vals[rep][t][RTIdx] = actuals.vals[rep][t][it->second] * futureInfoWeights[t-beg] + DAForecasts->vals[rep][t][DAIdx] * (1-futureInfoWeights[t-beg]);
+//		}
+//
+//		// update simulations
+//		if (simulations.size() > 0) {
+//			// get the correct indices
+//			DAIdx = DASimulations->mapVarNamesToIndex[ it->first ];
+//			RTIdx = RTSimulations->mapVarNamesToIndex[ it->first ];
+//
+//			// update the simulations
+//			for (int s=0; s<runParam.numTotScen; s++) {
+//				for (int t=beg; t<=end; t++) {
+//					RTSimulations->vals[s][t][RTIdx] = actuals.vals[rep][t][it->second] * futureInfoWeights[t-beg] + DASimulations->vals[s][t][DAIdx] * (1-futureInfoWeights[t-beg]);
+//				}
+//			}
+//		}
+//	}
 }
 
 
