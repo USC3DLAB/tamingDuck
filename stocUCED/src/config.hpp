639a77a02987c28289283ec4bc73b69ccd49a6b3
Harsha Gangammanavar <gjharsha@gmail.com>
2019-05-26 17:18:23 -0500

    Updated the UC models

--- stocUCED/src/config.hpp
+++ stocUCED/src/config.hpp
@@ -73,7 +73,10 @@ struct runType {
 	double 	resPerc_UC;				// spinning reserve percentages
 	double 	resPerc_ED;
 	double	renewableCoef;			// renewable-supply will be multiplied with this factor
+
 	double	storageCoef;			// storage capacity will be multiplied with this factor
+	double  storageDev;				// Allowable deviation from higher hierarchy optimization solution
+
 	double	rampingCoef;			// ramping rates will be multiplied with this factor
 	bool	updateForecasts;		// real-time forecast updates
 };
